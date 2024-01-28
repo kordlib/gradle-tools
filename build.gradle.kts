978c72a01987ed98ae38c4c65f300464d96f9a3e
Michael Rittmeister <michael@rittmeister.in>
2024-01-29 00:34:24 +0100

    Initial Release

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,3 @@
-import kotlin.io.encoding.Base64
-import kotlin.io.encoding.ExperimentalEncodingApi
-
 plugins {
     `java-gradle-plugin`
     `kotlin-dsl`
@@ -9,7 +6,7 @@ plugins {
 }
 
 group = "dev.kord"
-version = "1.0.8"
+version = "1.1.0"
 
 repositories {
     mavenCentral()
@@ -43,17 +40,14 @@ gradlePlugin {
 publishing {
     repositories {
         maven("artifactregistry://europe-west3-maven.pkg.dev/mik-music/kord") {
-//            credentials {
-//                username = "_json_key_base64"
-//                password = System.getenv("GOOGLE_KEY")?.toByteArray()?.let {
-//                    @OptIn(ExperimentalEncodingApi::class)
-//                    Base64.encode(it)
-//                }
-//            }
-//
-//            authentication {
-//                create<BasicAuthentication>("basic")
-//            }
+            credentials {
+                username = "_json_key_base64"
+                password = System.getenv("GOOGLE_KEY")
+            }
+
+            authentication {
+                create<BasicAuthentication>("basic")
+            }
         }
     }
 }
