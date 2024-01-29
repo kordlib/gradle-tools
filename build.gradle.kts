1b20d8bb057c8c8917076dbab1e0b6faaa0ac3d6
Michael Rittmeister <michael@rittmeister.in>
2024-01-29 22:50:51 +0100

    Fix versioning error

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "dev.kord"
-version = "1.2.1"
+version = "1.2.2"
 
 repositories {
     mavenCentral()
--- src/main/java/Versioning.kt
+++ src/main/java/Versioning.kt
@@ -3,6 +3,6 @@ package dev.kord.gradle.tools
 import dev.kord.gradle.tools.util.libraryVersion
 import org.gradle.api.Project
 
-fun Project.applyVersioning() {
+fun Project.applyVersioning() = afterEvaluate {
     version = libraryVersion
 }
